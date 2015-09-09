d51ba30dee1a4d19f85ddf2766c6dc01f11814a4
Brice Person <brice@ideeslibres.org>
2015-09-09 14:24:11 +0200

    Back to more crossbrowser css things

--- summarize-pad.js
+++ summarize-pad.js
@@ -1,10 +1,48 @@
-(function(){
-  var link=document.createElement("link");
-  link.rel='stylesheet';
-  link.type='text/css';
-  link.href="https://raw.githubusercontent.com/bjperson/summarize-pad/master/css/main.css?rand="+(Math.round(Math.random()*1000));
-  document.head.appendChild(link);
-})()
+var style = document.createElement('style');
+style.textContent = " \
+#padassistant {  \
+  background-color: #fff;  \
+  border-left: 1px solid #dfdfdf;  \
+  font-size: 1.5em;  \
+  height: 100%;  \
+  overflow: auto;  \
+  position: absolute;  \
+  right: 0;  \
+  width: 30%;  \
+  z-index: 100;  \
+}  \
+#toggleassistant {  \
+  background-color: #e8e9e9;  \
+  border: 1px solid #dfdfdf;  \
+  color: #fff;  \
+  font-size: 2.5em;  \
+  font-weight: bold;  \
+  height: 32px;  \
+  overflow: auto;  \
+  position: absolute;  \
+  right: 0;  \
+  text-align: center;  \
+  width: 32px;  \
+  z-index: 101;  \
+  cursor:pointer;  \
+}  \
+.tasktype {  \
+  color:#000;  \
+  background-color:#e8e9e9;  \
+  padding:0.8em;  \
+  font-weight:bold;  \
+}  \
+.task {  \
+  border-bottom:1px solid #dfdfdf;  \
+  padding:0.3em;  \
+  cursor:pointer;  \
+}  \
+.task:hover {  \
+  background-color: #F2F7F7;  \
+}";
+document.head.appendChild(style);
+
+help = ''
 
 function summarize() {
   
@@ -13,6 +51,8 @@ function summarize() {
   regtype = '^[A-Z :]{2,}$';
   
   regflag = /\[([^\]]{2,})\]/g;
+  
+  countflag = ':$';
   
   list = {}
   
@@ -52,17 +92,19 @@ function summarize() {
   else {
     top = $('#editbar').outerHeight();
     $('#editorcontainer').prepend('<div id="padassistant" style="top: '+top+'px;"></div>');
-    $('#editorcontainer').prepend('<div id="toggleassistant" onclick="javascript:toggleAssistant()" style="top: '+top+'px;">@</div>');
-    
+    $('#editorcontainer').prepend('<div id="toggleassistant" title="Open/Close" onclick="javascript:toggleAssistant()" style="top: '+top+'px;">#</div>');
   }
   
   for (item in list) {
     if (list[item].tasks.length > 0) {
-      $('#padassistant').append('<div class="tasktype">'+list[item].tasktype+'</div>');
+      tasktype = list[item].tasktype;
+      if (tasktype.match(countflag)) { tasktype += ' '+list[item].tasks.length; }
+      $('#padassistant').append('<div class="tasktype">'+tasktype+'</div>');
       for (task in list[item].tasks) {
-        if (list[item].tasks[task].text.match(regflag)) { color = colors[list[item].tasks[task].text.match(regflag)[0]]; }
+        text = list[item].tasks[task].text;
+        if (text.match(regflag)) { color = colors[text.match(regflag)[0]]; }
         else { color = 'gray';}
-        $('#padassistant').append('<div class="task" onclick="javascript:goToTask(\''+list[item].tasks[task].top+'\')" style="color:'+color+';">'+list[item].tasks[task].text+'</div>');
+        $('#padassistant').append('<div class="task" onclick="javascript:goToTask(\''+list[item].tasks[task].top+'\')" style="color:'+color+';">'+text+'</div>');
       }
     }
   }

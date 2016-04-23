cd637be8f82e2c1d4827092def13dd64c457e0ec
Nicolas Joyard <nicolas@njoyard.fr>
2016-04-23 11:43:57 +0200

    [etherpad-lite] Cleanup code

--- summarize-pad.js
+++ summarize-pad.js
@@ -11,174 +11,177 @@
 // @description Parse any etherpad content to summarize it with simple rules
 // ==/UserScript==
 
-var style = document.createElement('style');
-style.textContent = " \
-#padassistant {  \
-  background-color: #fff;  \
-  border-left: 1px solid #dfdfdf;  \
-  font-size: 1.5em;  \
-  height: 100%;  \
-  overflow: auto;  \
-  position: absolute;  \
-  right: 0;  \
-  width: 30%;  \
-  z-index: 100;  \
-}  \
-#toggleassistant {  \
-  background-color: #e8e9e9;  \
-  border: 1px solid #dfdfdf;  \
-  color: #fff;  \
-  font-size: 2.5em;  \
-  font-weight: bold;  \
-  height: 32px;  \
-  overflow: auto;  \
-  position: absolute;  \
-  right: 0;  \
-  text-align: center;  \
-  width: 32px;  \
-  z-index: 101;  \
-  cursor:pointer;  \
-}  \
-.tasktype {  \
-  color:#000;  \
-  background-color:#e8e9e9;  \
-  padding:0.8em;  \
-  font-weight:bold;  \
-}  \
-.task {  \
-  border-bottom:1px solid #dfdfdf;  \
-  padding:0.3em;  \
-  cursor:pointer;  \
-}  \
-.task:hover {  \
-  background-color: #F2F7F7;  \
-} \
-#help { \
-  padding: 1em; \
-  border-top: 1px solid #dfdfdf; \
-} \
-#help h1 { \
-  font-weight: bold; \
-  margin-bottom: 0.5em; \
-}";
-document.head.appendChild(style);
+(function() {
+  var style = document.createElement('style');
 
-help = '<div id="help" style="display: none"> \
-  <h1>Aide</h1> \
-  <p> \
-  Une section doit être soulignée, en majuscule et ne doit pas comporter de couleurs d\'auteurs, exemple : \
-  <br /> \
-  <u>SECTION</u><br /> \
-  <br /> \
-  Si une section se termine par ":" les tâches seront comptées et affichées, exemple : \
-<br /><u>SECTION :</u><br /> \
-  <br /> \
-  Si une section ne comporte pas de tâches, elle ne sera pas affichée. \
-  <br /><br /> \
-  </p> \
-  <p>  \
-  Une tâche est un élément de liste à puce de premier niveau avec ou sans couleurs, exemple : \
-  <br /> \
-<ul><li><span style="background-color:#FFC7F1">Tâche</span></li></ul> \
-  </p> \
-</div>';
+  style.textContent = " \
+  #padassistant {  \
+    background-color: #fff;  \
+    border-left: 1px solid #dfdfdf;  \
+    font-size: 1.5em;  \
+    height: 100%;  \
+    overflow: auto;  \
+    position: absolute;  \
+    right: 0;  \
+    width: 30%;  \
+    z-index: 100;  \
+  }  \
+  #toggleassistant {  \
+    background-color: #e8e9e9;  \
+    border: 1px solid #dfdfdf;  \
+    color: #fff;  \
+    font-size: 2.5em;  \
+    font-weight: bold;  \
+    height: 32px;  \
+    overflow: auto;  \
+    position: absolute;  \
+    right: 0;  \
+    text-align: center;  \
+    width: 32px;  \
+    z-index: 101;  \
+    cursor:pointer;  \
+  }  \
+  .tasktype {  \
+    color:#000;  \
+    background-color:#e8e9e9;  \
+    padding:0.8em;  \
+    font-weight:bold;  \
+  }  \
+  .task {  \
+    border-bottom:1px solid #dfdfdf;  \
+    padding:0.3em;  \
+    cursor:pointer;  \
+  }  \
+  .task:hover {  \
+    background-color: #F2F7F7;  \
+  } \
+  #help { \
+    padding: 1em; \
+    border-top: 1px solid #dfdfdf; \
+  } \
+  #help h1 { \
+    font-weight: bold; \
+    margin-bottom: 0.5em; \
+  }";
 
-function summarize() {
+  document.head.appendChild(style);
+
+  var help = '<div id="help" style="display: none"> \
+    <h1>Aide</h1> \
+    <p> \
+    Une section doit être soulignée, en majuscule et ne doit pas comporter de couleurs d\'auteurs, exemple : \
+    <br /> \
+    <u>SECTION</u><br /> \
+    <br /> \
+    Si une section se termine par ":" les tâches seront comptées et affichées, exemple : \
+  <br /><u>SECTION :</u><br /> \
+    <br /> \
+    Si une section ne comporte pas de tâches, elle ne sera pas affichée. \
+    <br /><br /> \
+    </p> \
+    <p>  \
+    Une tâche est un élément de liste à puce de premier niveau avec ou sans couleurs, exemple : \
+    <br /> \
+  <ul><li><span style="background-color:#FFC7F1">Tâche</span></li></ul> \
+    </p> \
+  </div>';
+
   var isLite = !($(".EtherpadLink").length);
-  var data;
+  var regtype = '^[A-Z :]{2,}$';
+  var regflag = /\[([^\]]{2,})\]/g;
+  var countflag = ':$';
 
-  if (isLite) {
-    data = $('iframe').contents().find('iframe').contents();
-  } else {
-    data = $(document.getElementsByTagName('iframe')[1]).contents().find('iframe').contents();
-  }
-
-  regtype = '^[A-Z :]{2,}$';
-
-  regflag = /\[([^\]]{2,})\]/g;
-
-  countflag = ':$';
-
-  list = {}
-
-  colors = {
+  var colors = {
     '[URGENT]':'red',
     '[RDV]':'green',
     '[CONF]':'blue',
     '[PARL]':'violet'
   };
 
-  $.each($(data).find("div[id*='magicdomid']"), function() {
-    that = $(this);
-    id = this.id;
-    text = that.text();
-    if(text.match(regtype)) {
-      if (that.contents().find('u').length === 1) {
-        list[id] = {'tasktype':text, 'tasks':[]}
+  function getMainIframe() {
+    return isLite ? $('iframe').contents() : $(document.getElementsByTagName('iframe')[1]).contents();
+  }
+
+  function summarize() {
+    var data = getMainIframe().find('iframe').contents();
+    data.find('body').css('width', '69%');
+
+    var list = {};
+
+    $.each($(data).find("div[id*='magicdomid']"), function() {
+      that = $(this);
+      id = this.id;
+      text = that.text();
+      if(text.match(regtype)) {
+        if (that.contents().find('u').length === 1) {
+          list[id] = {'tasktype':text, 'tasks':[]}
+        }
+      }
+    });
+
+    $.each($(data).find("div[id*='magicdomid']"), function() {
+      if (list[this.id] !== undefined) {
+        listid = this.id;
+      }
+      if (typeof listid != 'undefined') {
+        if ($(this).find('ul').hasClass('list-bullet1')) {
+          list[listid].tasks.push({'id':this.id,'text':$(this).text(),'top':this.getBoundingClientRect().top});
+        }
+      }
+
+    });
+
+    if ($('#padassistant').length === 1) {
+      $('#padassistant').html('');
+    }
+    else {
+      top = $('#editbar').outerHeight();
+      $('#editorcontainer').prepend('<div id="padassistant" style="top: '+top+'px;"></div>');
+      $('#editorcontainer').prepend('<div id="toggleassistant" title="Open/Close" onclick="javascript:toggleAssistant()" style="top: '+top+'px;">#</div>');
+    }
+
+    for (item in list) {
+      if (list[item].tasks.length > 0) {
+        var tasktype = list[item].tasktype;
+        if (tasktype.match(countflag)) { tasktype += ' '+list[item].tasks.length; }
+        $('#padassistant').append('<div class="tasktype">'+tasktype+'</div>');
+        for (task in list[item].tasks) {
+          var text = list[item].tasks[task].text;
+          var color;
+
+          if (text.match(regflag)) { color = colors[text.match(regflag)[0]]; }
+          else { color = 'gray';}
+          $('#padassistant').append('<div class="task" onclick="javascript:goToTask(\''+list[item].tasks[task].top+'\')" style="color:'+color+';">'+text+'</div>');
+        }
       }
     }
-  });
-
-  $.each($(data).find("div[id*='magicdomid']"), function() {
-    if (list[this.id] !== undefined) {
-      listid = this.id;
-    }
-    if (typeof listid != 'undefined') {
-      if ($(this).find('ul').hasClass('list-bullet1')) {
-        list[listid].tasks.push({'id':this.id,'text':$(this).text(),'top':this.getBoundingClientRect().top});
-      }
-    }
-
-  });
-
-  if ($('#padassistant').length === 1) {
-    $('#padassistant').html('');
-  }
-  else {
-    top = $('#editbar').outerHeight();
-    $('#editorcontainer').prepend('<div id="padassistant" style="top: '+top+'px;"></div>');
-    $('#editorcontainer').prepend('<div id="toggleassistant" title="Open/Close" onclick="javascript:toggleAssistant()" style="top: '+top+'px;">#</div>');
+    $('#padassistant').append('<div style="background-color:#e8e9e9;cursor:pointer;text-align:center;" onclick="javascript:openHelp()">?</div>'+help);
   }
 
-  for (item in list) {
-    if (list[item].tasks.length > 0) {
-      tasktype = list[item].tasktype;
-      if (tasktype.match(countflag)) { tasktype += ' '+list[item].tasks.length; }
-      $('#padassistant').append('<div class="tasktype">'+tasktype+'</div>');
-      for (task in list[item].tasks) {
-        text = list[item].tasks[task].text;
-        if (text.match(regflag)) { color = colors[text.match(regflag)[0]]; }
-        else { color = 'gray';}
-        $('#padassistant').append('<div class="task" onclick="javascript:goToTask(\''+list[item].tasks[task].top+'\')" style="color:'+color+';">'+text+'</div>');
-      }
+  function goToTask(top) {
+    getMainIframe.contents().find("html, body").animate({ scrollTop: top }, { duration: 'medium', easing: 'swing' });
+  }
+
+  function closePA() {
+    $('#padassistant').remove();
+  }
+
+  function toggleAssistant() {
+    if ($('#padassistant').length === 1) {
+      clearInterval(refresh);
+      closePA();
+      getMainIframe.find('iframe').contents().find('body').css('width', '100%');
+    }
+    else {
+      summarize();
+      refresh = setInterval(summarize, 5000);
     }
   }
-  $('#padassistant').append('<div style="background-color:#e8e9e9;cursor:pointer;text-align:center;" onclick="javascript:openHelp()">?</div>'+help);
-}
 
-function goToTask(top) {
-  $(document.getElementsByTagName('iframe')[1]).contents().find("html, body").animate({ scrollTop: top }, { duration: 'medium', easing: 'swing' });
-}
-
-function closePA() {
-  $('#padassistant').remove();
-}
-
-function toggleAssistant() {
-  if ($('#padassistant').length === 1) {
-    clearInterval(refresh);
-    closePA();
-    $(document.getElementsByTagName('iframe')[1]).contents().find('iframe').contents().find('body').css('width', '100%');
+  function openHelp() {
+    $('#help').toggle();
   }
-  else {
-    summarize();
-    refresh = setInterval(summarize, 5000);
-  }
-}
 
-function openHelp() {
-  $('#help').toggle();
-}
-
-summarize();
-var refresh = setInterval(summarize, 5000);
+  summarize();
+  var refresh = setInterval(summarize, 5000);
+}());

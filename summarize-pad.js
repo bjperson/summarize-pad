ce7ed1023623a7cc3821013096e1a70225f9b285
bjperson <brice@ideeslibres.org>
2016-05-18 17:00:24 +0200

    Add tag setup, improvements and english help

--- summarize-pad.js
+++ summarize-pad.js
@@ -2,11 +2,12 @@
 // @name summarize-pad
 // @namespace https://github.com/bjperson/summarize-pad
 // @author Brice Person
-// @date 09/09/2015
-// @version 1
-// @license AGPL V3; http://en.wikipedia.org/wiki/WTF_Public_License
-// @include https://regardscitoyens.framapad.org/*
-// @include https://pad.lqdn.fr/p/*
+// @date 18/05/2015
+// @version 1.5
+// @license AGPL V3; https://en.wikipedia.org/wiki/GNU_Affero_General_Public_License
+// @include https://pad.regardscitoyens.org/*
+// @include https://framapad.org/*
+// @include https://pad.lqdn.fr/*
 // @compat Firefox, Chrome
 // @description Parse any etherpad content to summarize it with simple rules
 // ==/UserScript==
@@ -33,7 +34,7 @@ var summarizePad = (function() {
     font-size: 2.5em;  \
     font-weight: bold;  \
     height: 32px;  \
-    overflow: auto;  \
+    overflow: hidden;  \
     position: absolute;  \
     right: 0;  \
     text-align: center;  \
@@ -67,28 +68,38 @@ var summarizePad = (function() {
   document.head.appendChild(style);
 
   var help = '<div id="help" style="display: none"> \
-    <h1>Aide</h1> \
+    <h2>Help</h2> \
     <p> \
-    Une section doit être soulignée, en majuscule et ne doit pas comporter de couleurs d\'auteurs, exemple : \
+    A list must be uppercase with an underline, example : \
     <br /> \
-    <u>SECTION</u><br /> \
+    <u>LIST</u><br /> \
     <br /> \
-    Si une section se termine par ":" les tâches seront comptées et affichées, exemple : \
-  <br /><u>SECTION :</u><br /> \
+    If a list title ends with ":", tasks will be counted and displayed, example : \
+    <br /><u>LIST :</u><br /> \
     <br /> \
-    Si une section ne comporte pas de tâches, elle ne sera pas affichée. \
+    If a list does not contain tasks, it will not be displayed. \
     <br /><br /> \
     </p> \
     <p>  \
-    Une tâche est un élément de liste à puce de premier niveau avec ou sans couleurs, exemple : \
+    A task is a top-level bullet list item, example : \
     <br /> \
-  <ul><li><span style="background-color:#FFC7F1">Tâche</span></li></ul> \
+    <ul><li><span style="background-color:#FFC7F1;">Task</span></li></ul> \
+    </p> \
+    <p><br />  \
+    You can setup tags writing anything like this anywhere on your pad : \
+    <br /> \
+    [URGENT]: red; <br />\
+    [DONE]: #E5E5E5; \
+    </p> \<br /><p><hr  style="color:gray;"/>  \
+    <span style="color:gray;font-size:0.8em;">Powered by <a href="https://github.com/bjperson/summarize-pad">Summarize-pad</a></span> \
     </p> \
   </div>';
 
   var isLite = !($(".EtherpadLink").length);
   var regtype = '^[A-Z :]{2,}$';
   var regflag = /\[([^\]]{2,})\]/g;
+  var colorflag = /^(\[[^\]]{2,}\]):/;
+  var flagcolor = /\]: ([^;]{2,});/;
   var countflag = ':$';
 
   var colors = {
@@ -117,6 +128,11 @@ var summarizePad = (function() {
           list[id] = {'tasktype':text, 'tasks':[]}
         }
       }
+      if(text.match(colorflag)) {
+        if(text.match(flagcolor)) {
+          colors[text.match(colorflag)[1]] = text.match(flagcolor)[1];
+        }
+      }
     });
 
     $.each($(data).find("div[id*='magicdomid']"), function() {
@@ -131,31 +147,31 @@ var summarizePad = (function() {
 
     });
 
-    if ($('#padassistant').length === 1) {
-      $('#padassistant').html('');
+    if ($('#assistanttasks').length === 1) {
+      $('#assistanttasks').html('');
     }
     else {
       top = $('#editbar').outerHeight();
-      $('#editorcontainer').prepend('<div id="padassistant" style="top: '+top+'px;"></div>');
+      $('#editorcontainer').prepend('<div id="padassistant" style="top: '+top+'px;"><div id="assistanttasks"></div></div>');
       $('#editorcontainer').prepend('<div id="toggleassistant" title="Open/Close" onclick="javascript:summarizePad.toggleAssistant()" style="top: '+top+'px;">#</div>');
+      $('#padassistant').append('<div style="background-color:#e8e9e9;cursor:pointer;text-align:center;" onclick="javascript:summarizePad.openHelp()">?</div>'+help);
     }
 
     for (item in list) {
       if (list[item].tasks.length > 0) {
         var tasktype = list[item].tasktype;
         if (tasktype.match(countflag)) { tasktype += ' '+list[item].tasks.length; }
-        $('#padassistant').append('<div class="tasktype">'+tasktype+'</div>');
+        $('#assistanttasks').append('<div class="tasktype">'+tasktype+'</div>');
         for (task in list[item].tasks) {
           var text = list[item].tasks[task].text;
           var color;
 
           if (text.match(regflag)) { color = colors[text.match(regflag)[0]]; }
           else { color = 'gray';}
-          $('#padassistant').append('<div class="task" onclick="javascript:summarizePad.goToTask(\''+list[item].tasks[task].top+'\')" style="color:'+color+';">'+text+'</div>');
+          $('#assistanttasks').append('<div class="task" onclick="javascript:summarizePad.goToTask(\''+list[item].tasks[task].top+'\')" style="color:'+color+';">'+text+'</div>');
         }
       }
     }
-    $('#padassistant').append('<div style="background-color:#e8e9e9;cursor:pointer;text-align:center;" onclick="javascript:summarizePad.openHelp()">?</div>'+help);
   }
 
   function goToTask(top) {
